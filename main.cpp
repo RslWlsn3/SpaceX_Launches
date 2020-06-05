843d10553b8425b4dc304cc15e65fbb2d1480ec1
CMote <connor.mote@intimetec.com>
2020-06-05 09:09:10 -0600

    Added class SpaceXLaunches

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@
 
 using json = nlohmann::json;
 
-//perform cmd command return result
+const char* upcomingLaunchesCmd = "curl --location --request GET 'https://api.spacexdata.com/v3/launches/upcoming'";
+
+//perform api call and return results
 std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
     std::string result;
@@ -30,48 +32,48 @@ struct LaunchData{
     std::string launch_date_local;
     LaunchData(int fn, std::string yr, std::string ldl): flight_number(fn), launch_year(yr), launch_date_local(ldl)
     {        
-    }
-    
+    }    
 };
 
-//creates and returns and std::map with the keys being the name of the mission and vaules being a LaunchData obj. 
-std::map<std::string, LaunchData*> createMap(const json &j){    
-    std::map<std::string, LaunchData*> launchMap;    
-      for (auto& element : j) {  
-          auto el = element["tentative_max_precision"];            
-      LaunchData* l = new LaunchData(element["flight_number"],element["launch_year"],element["launch_date_local"]);          
-      launchMap[element["mission_name"]] = l;
-    }
-return launchMap;
-}
-
-//Display stored launch data
-void printLaunchData(std::map<std::string, LaunchData*> &launchMap){
-    for (std::map<std::string, LaunchData*>::iterator it = launchMap.begin();it != launchMap.end();it++)
-        {
-            std::cout<< "mission_name: " << it->first<<std::endl;
-            LaunchData* L = it->second;
-            std::cout<<"  -  flight_number: " << (*L).flight_number<<std::endl;
-            std::cout<<"  -  launch_year: " << (*L).launch_year<<std::endl;
-            std::cout<<"  -  launch_date_local: " << (*L).launch_date_local<<std::endl;
-            std::cout<<"------------------------------\n";
+class SpaceXLaunches{
+    private:
+        std::map<std::string, LaunchData*> launchMap;
+    public:
+        //takes in parsed json data from api call and poplulates a std::map. 
+        SpaceXLaunches(const json &j) {
+            for (auto& element : j) {  
+                auto el = element["tentative_max_precision"];            
+                LaunchData* l = new LaunchData(element["flight_number"],element["launch_year"],element["launch_date_local"]);          
+                launchMap[element["mission_name"]] = l;
+                }            
         }
-}
 
-//delete heap allocated LaunchData objs
-void cleanUp(std::map<std::string, LaunchData*> launchMap)
-{
-    for (std::map<std::string, LaunchData*>::iterator it = launchMap.begin();it != launchMap.end();it++)
-        {
-            LaunchData* L = it->second;
-            delete L;
+        //Display stored launch data
+        void displayLaunchData(){
+            for (std::map<std::string, LaunchData*>::iterator it = launchMap.begin();it != launchMap.end();it++)
+                {
+                    std::cout<< "mission_name: " << it->first<<std::endl;
+                    LaunchData* L = it->second;
+                    std::cout<<"  -  flight_number: " << (*L).flight_number<<std::endl;
+                    std::cout<<"  -  launch_year: " << (*L).launch_year<<std::endl;
+                    std::cout<<"  -  launch_date_local: " << (*L).launch_date_local<<std::endl;
+                    std::cout<<"------------------------------\n";
+                }
         }
-}
+
+        //delete heap allocated LaunchData objs
+        ~SpaceXLaunches(){
+            for (std::map<std::string, LaunchData*>::iterator it = launchMap.begin();it != launchMap.end();it++)
+                {
+                    LaunchData* L = it->second;
+                    delete L;
+                }
+        } 
+};
 
 int main(){
-  std::string launch_json = exec("curl --location --request GET 'https://api.spacexdata.com/v3/launches/upcoming'");  
+  std::string launch_json = exec(upcomingLaunchesCmd);  
   auto j = json::parse(launch_json); 
-  std::map<std::string, LaunchData*> launchMap = createMap(j);
-  printLaunchData(launchMap);
-  cleanUp(launchMap);  
+  SpaceXLaunches spaceX(j);
+  spaceX.displayLaunchData();
 }
