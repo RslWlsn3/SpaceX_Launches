9bac9244c1014eb2f0c130650f8f8b602bf926c4
CMote <connor.mote@intimetec.com>
2020-06-09 09:53:50 -0600

    Made changes to reflect Cody's working fork

--- build/src/$(CMAKE_CURRENT_SOURCE_DIR)/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/connor.mote/dev/spacexlaunches/json
-# Build directory: /mnt/c/Users/connor.mote/dev/spacexlaunches/build/src/$(CMAKE_CURRENT_SOURCE_DIR)
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
